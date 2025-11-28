331629157cc955b1fc3fe444fa4fee06c134a64c
yuriy-bak <90099251+yuriy-bak@users.noreply.github.com>
2025-11-28 08:10:01 +0300

    ci

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -39,10 +39,10 @@ android {
 
     signingConfigs {
         create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String
             keyPassword = keystoreProperties["keyPassword"] as String
             storeFile = file(keystoreProperties["storeFile"] as String)
             storePassword = keystoreProperties["storePassword"] as String
+            keyAlias = keystoreProperties["keyAlias"] as String
         }
     }
 
